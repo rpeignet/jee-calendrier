88d9470a9cb567a5f7ba8a6ce586483dace6d3bc
robin peignet <robin.peignet.contact@gmail.com>
2024-07-12 10:12:03 +0200

    validation creation utilisateur

--- calendrier_ybr_rpt/src/main/java/fr/esgi/calendrier_ybr_rpt/controller/rest/advice/UtilisateurRestControllerAdvice.java
+++ calendrier_ybr_rpt/src/main/java/fr/esgi/calendrier_ybr_rpt/controller/rest/advice/UtilisateurRestControllerAdvice.java
@@ -2,6 +2,7 @@ package fr.esgi.calendrier_ybr_rpt.controller.rest.advice;
 
 import fr.esgi.calendrier_ybr_rpt.exception.utilisateur.ThemeNotFoundException;
 import fr.esgi.calendrier_ybr_rpt.exception.utilisateur.UtilisateurNotFoundException;
+import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,6 +19,12 @@ public class UtilisateurRestControllerAdvice {
     @ExceptionHandler(UtilisateurNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleUtilisateurNotFoundException(UtilisateurNotFoundException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleValidationException(ValidationException e){
         return e.getMessage();
     }
 }
--- calendrier_ybr_rpt/src/main/java/fr/esgi/calendrier_ybr_rpt/service/impl/UtilisateurServiceImpl.java
+++ calendrier_ybr_rpt/src/main/java/fr/esgi/calendrier_ybr_rpt/service/impl/UtilisateurServiceImpl.java
@@ -5,6 +5,9 @@ import fr.esgi.calendrier_ybr_rpt.exception.MissingParamException;
 import fr.esgi.calendrier_ybr_rpt.exception.utilisateur.UtilisateurNotFoundException;
 import fr.esgi.calendrier_ybr_rpt.repository.UtilisateurRepository;
 import fr.esgi.calendrier_ybr_rpt.service.UtilisateurService;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ValidationException;
+import jakarta.validation.Validator;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -20,9 +24,20 @@ public class UtilisateurServiceImpl implements UtilisateurService {
     private UtilisateurRepository utilisateurRepository;
     private InMemoryUserDetailsManager inMemoryUserDetailsManager;
     private PasswordEncoder passwordEncoder;
+    private Validator validator;
 
     @Override
     public Utilisateur save(Utilisateur utilisateur){
+        Set<ConstraintViolation<Utilisateur>> violations = validator.validate(utilisateur);
+
+        if (!violations.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            for (ConstraintViolation<Utilisateur> violation : violations) {
+                sb.append(violation.getMessage());
+            }
+            throw new ValidationException(sb.toString());
+        }
+
         utilisateur.setNombreDePoint(500);
         UserDetails newUser = User.builder().username(utilisateur.getEmail())
                 .password(passwordEncoder.encode(utilisateur.getMotDePasse()))
