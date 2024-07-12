fc8f6ae1f99195e4d060c0b29aa75911867ba760
robin peignet <robin.peignet.contact@gmail.com>
2024-07-12 09:55:45 +0200

    fix ex

--- calendrier_ybr_rpt/src/main/java/fr/esgi/calendrier_ybr_rpt/exception/gif/GifDownloadException.java
+++ calendrier_ybr_rpt/src/main/java/fr/esgi/calendrier_ybr_rpt/exception/gif/GifDownloadException.java
@@ -2,6 +2,6 @@ package fr.esgi.calendrier_ybr_rpt.exception.gif;
 
 public class GifDownloadException extends RuntimeException{
     public GifDownloadException(){
-        super("Erreur lors du téléchargement du fichier GIF.");
+        super("Le Gif distant doit pointer vers une URL au bon format qui se termine par .gif, .Gif ou .GIF");
     }
 }
